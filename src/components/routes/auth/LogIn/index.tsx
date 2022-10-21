b15c32d11d7bf04c2fbdaac277e568b64a6439f4
Dionis Uliu <76048812+DionisUliu@users.noreply.github.com>
2022-10-21 13:30:01 +0200

    Login UI and new Google Auth button style (#2)

--- src/components/routes/auth/LogIn/index.tsx
+++ src/components/routes/auth/LogIn/index.tsx
@@ -1,13 +1,36 @@
-import axios from 'axios';
-import { ReactElement, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { ReactElement } from 'react';
 import LogInButton from './LogInButton';
 import styles from './index.module.scss';
 
 const LogIn = (): ReactElement => {
   return (
     <div className={styles.container}>
-      <LogInButton />
+      <div className={styles.splitScreenLeft}>
+        <div className={styles.centered}>
+          <img
+            className={styles.logoStyle}
+            src="/images/logo_softup.svg"
+            alt="softup-logo"
+          />
+          <h1 className={styles.titleLoginStyle}>Welcome to BoothUp</h1>
+          <p className={styles.descriptionLoginStyle}>
+            Login with your Softup workspace account.
+          </p>
+          <LogInButton />
+        </div>
+      </div>
+
+      <div className={styles.splitScreenRight}>
+        <div className={styles.imgContainer}>
+          <div className={styles.centered}>
+            <img
+              className={styles.gifStyle}
+              src="/images/calendar_thin.gif"
+              alt="gif"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
