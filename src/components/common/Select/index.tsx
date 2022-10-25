84c7e034ba29850b9051de50a102591807435271
Enri Rane <enrirane@gmail.com>
2022-10-25 15:53:45 +0200

    Finished the UI of Reservation modal

--- src/components/common/Select/index.tsx
+++ src/components/common/Select/index.tsx
@@ -9,24 +9,23 @@ import styles from './index.module.scss';
 const { Option } = Select;
 
 type OptionProps = {
-  label: string,
-  value: string | number,
-  key: number,
+  label: string;
+  value: string | number;
+  key: number;
 };
 
 type SelectPropsType = {
-  name: string,
-  placeholder?: string,
-  disabled?: boolean,
-  options: OptionProps[],
-  error?: string,
-  touched?: boolean,
-  value?: string | number,
-  style?: any,
-  allowClear?: boolean,
-  mode?: 'multiple' | 'tags' | undefined,
-  defaultValue?: string | number,
-  onChange?: (e?:any) => void
+  name: string;
+  placeholder?: string;
+  disabled?: boolean;
+  options: OptionProps[];
+  error?: string;
+  value?: string | number;
+  style?: any;
+  allowClear?: boolean;
+  mode?: 'multiple' | 'tags' | undefined;
+  defaultValue?: string | number;
+  onChange?: (e?: any) => void;
 };
 
 const CustomSelect = (props: SelectPropsType): ReactElement => {
@@ -34,31 +33,28 @@ const CustomSelect = (props: SelectPropsType): ReactElement => {
     if (!touched) {
       return null;
     }
-    return error
-      ? (
-        <Tooltip title={error}>
-          <FontAwesomeIcon
-            className={styles.SuffixIcon}
-            icon={faXmark}
-            color={colors.danger}
-          />
-        </Tooltip>
-      )
-      : (
+    return error ? (
+      <Tooltip title={error}>
         <FontAwesomeIcon
           className={styles.SuffixIcon}
-          icon={faCheck}
-          color={colors.success}
+          icon={faXmark}
+          color={colors.danger}
         />
-      );
+      </Tooltip>
+    ) : (
+      <FontAwesomeIcon
+        className={styles.SuffixIcon}
+        icon={faCheck}
+        color={colors.success}
+      />
+    );
   };
-  const suffix = <>{getSuffixIcon(props.error, props.touched)}</>;
+  // const suffix = <>{getSuffixIcon(props.error, props.touched)}</>;
   const commonProps = {
     placeholder: props.placeholder,
     disabled: props.disabled,
     style: props.style,
     value: props.value,
-    touched: props.touched,
     allowClear: props.allowClear,
     mode: props.mode,
     name: props.name,
@@ -66,26 +62,7 @@ const CustomSelect = (props: SelectPropsType): ReactElement => {
     onChange: props.onChange,
   };
 
-  const backgroundColor = props.disabled ? '#F5F5F5' : 'white';
-
-  const options = props.options.map(
-    (option) => (
-      <Option
-        key={option.key}
-        value={option.value}
-      >
-        {option.label}
-      </Option>
-    ),
-  );
-  return (
-    <Select
-      className={`${styles.InputContainer} ${styles.select} ${((props.error || props.value === undefined) && props.touched) && styles.InputError}`}
-      {...commonProps}
-    >
-      {options}
-    </Select>
-  );
+  return <Select options={props.options} {...commonProps} />;
 };
 
 CustomSelect.defaultProps = {
@@ -93,7 +70,6 @@ CustomSelect.defaultProps = {
   placeholder: '',
   defaultValue: undefined,
   error: undefined,
-  touched: false,
   value: undefined,
   disabled: false,
   allowClear: false,
--- src/components/routes/app/Index/index.tsx
+++ src/components/routes/app/Index/index.tsx
@@ -1,7 +1,13 @@
 import { ReactElement } from 'react';
+import CustomModal from '../../../common/Modal';
+import ReservationModal from '../features/ReservationModal';
 
 const AppIndex = (): ReactElement => {
-  return <div>Where the map component and reservation will be</div>;
+  return (
+    <div>
+      <ReservationModal />
+    </div>
+  );
 };
 
 export default AppIndex;
