0f3232423f4c4f4be5b2ec3315577392c11e87b8
Dionis Uliu <76048812+DionisUliu@users.noreply.github.com>
2022-11-03 10:21:06 +0100

    Finished new UI and UI fixes (#11)

--- src/components/common/Spinner/index.tsx
+++ src/components/common/Spinner/index.tsx
@@ -1,22 +1,22 @@
 import { Spin } from 'antd';
+import { number } from 'yup/lib/locale';
 
 import styles from './index.module.scss';
 
 type SpinnerProps = {
-  loading?: boolean,
+  loading?: boolean;
+  tip?: string;
 };
 
 const CustomSpinner = (props: SpinnerProps) => (
   <div className={styles.SpinnerContainer}>
-    <Spin
-      size="large"
-      spinning={props.loading}
-    />
+    <Spin size="large" spinning={props.loading} tip={props.tip} />
   </div>
 );
 
 CustomSpinner.defaultProps = {
   loading: true,
+  tip: '',
 };
 
 export default CustomSpinner;
